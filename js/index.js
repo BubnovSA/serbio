d132b91a9668f24a06b784ba69b792572f5dd8e3
bubnovSA <bubnofwork@mail.ru>
2022-03-19 11:17:10 +0300

    IBS-1 Add file in repository

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const headerBurger = document.querySelector('.header__burger');
+
+const burgerTop = document.querySelector('.burger__item_top');
+const burgerMiddle = document.querySelector('.burger__item_middle');
+const burgerButton = document.querySelector('.burger__item_but');
+
+const container = document.querySelector('.header__menu');
+
+headerBurger.addEventListener('click', () => {
+    burgerTop.classList.toggle('active');
+    burgerMiddle.classList.toggle('active');
+    burgerButton.classList.toggle('active');
+    // header__burger.classList.toggle('active');
+    container.classList.toggle('active');
+})
