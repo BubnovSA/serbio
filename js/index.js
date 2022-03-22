d7bf77b8c0a47af39bacefb36d9f095f953bf424
bubnovSA <bubnofwork@mail.ru>
2022-03-22 23:48:41 +0300

    Change style

--- js/index.js
+++ js/index.js
@@ -12,4 +12,5 @@ headerBurger.addEventListener('click', () => {
     burgerButton.classList.toggle('active');
     // header__burger.classList.toggle('active');
     container.classList.toggle('active');
+    document.body.style.toggle.overflow = 'hidden';
 })
